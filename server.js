e3ce5a9cc1f3ec8797b3ca33d4ef2ce5b2eb782a
jayballa7 <Jenn_Ballard7@hotmail.com>
2020-04-10 23:14:43 -0700

    initialize

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+  });
