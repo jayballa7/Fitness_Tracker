e05186ea48746fcb5f94356f265bef6540d4bddf
Jennifer Ballard <jenn_ballard7@hotmail.com>
2020-05-14 14:22:31 -0700

    prepare to push to heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
